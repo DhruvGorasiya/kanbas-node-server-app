ae8d0a111f3d5e473f2ebfeee1d756e8f8d68498
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-11-15 03:13:14 -0500

    4.2.4

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,103 @@
+export default[
+    {
+      "_id": "A101",
+      "title": "Propulsion Assignment",
+      "course": "RS101",
+      "modules": "Multiple Modules",
+      "availableDate":"2024-05-06",
+      "untilDate":"2024-05-12",
+      "dueDate": "2024-05-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of propulsion systems. \nYou will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A102",
+      "title": "Combustion Analysis",
+      "course": "RS101",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-02",
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of combustion analysis. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A103",
+      "title": "Nozzle Design Project",
+      "course": "RS101",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-16",
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of nozzle design. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A201",
+      "title": "Aerodynamics Quiz",
+      "course": "RS102",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-05-12",
+      "availableDate": "2024-05-06",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of aerodynamics. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A202",
+      "title": "Flow Analysis",
+      "course": "RS102",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-02",
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of flow analysis. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A203",
+      "title": "Heating Analysis",
+      "course": "RS102",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-16",
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of heating analysis. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A301",
+      "title": "Structural Design Task",
+      "course": "RS103",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-05-12",
+      "availableDate": "2024-05-06",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of structural design. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A302",
+      "title": "Orbital Calculations",
+      "course": "RS103",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-02",
+
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18" ,
+      "points": 100,
+      "description": "This assignment will test your knowledge of orbital calculations. You will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    },
+    {
+      "_id": "A303",
+      "title": "Systems Engineering Exam",
+      "course": "RS103",
+      "modules": "Multiple Modules",
+      "dueDate": "2024-10-16",
+      "availableDate": "2024-09-18",
+      "untilDate": "2024-09-18",
+      "points": 100,
+      "description": "This assignment will test your knowledge of systems engineering. \nYou will be required to answer a series of questions and submit your answers by the due date. \nGood luck!"
+    }
+  ]
+  
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
 import cors from 'cors';
+import UserRoutes from "./Kanbas/Users/routes.js";
 
 
 const app = express()
+UserRoutes(app);
 app.use(express.json());
 app.use(cors());
 
