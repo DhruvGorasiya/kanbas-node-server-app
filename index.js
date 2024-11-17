e0e49b181814ac1ceeb84cb1af6f330ffc1b6bc3
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-11-17 18:00:28 -0500

    4.5

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import session from "express-session";
 import "dotenv/config";
 import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";  
 
 const app = express()
 
@@ -13,26 +15,32 @@ app.use(cors({
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
 }));
 
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
-    };
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+};
 }
+
 app.use(session(sessionOptions));
+  
 
 
 
 app.use(express.json());
 
 UserRoutes(app);
+CourseRoutes(app);
+ModuleRoutes(app);
 Hello(app)
 Lab5(app)
 
