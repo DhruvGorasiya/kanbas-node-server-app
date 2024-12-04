248cb8aa445d8e8d2b5650306817b3396259e358
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-12-04 00:38:05 -0500

    Update routes.js

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -103,7 +103,7 @@ export default function UserRoutes(app) {
     const signin = async (req, res) => {
         const { username, password } = req.body;
         const currentUser = await dao.findUserByCredentials(username, password);
-        console.log(currentUser)
+        console.log("this is the current user: ",currentUser)
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
